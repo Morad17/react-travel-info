9decc97d888667e5c2764749b57c836b91e952e4
Morad17 <morad123@hotmail.co.uk>
2022-03-09 19:53:03 +0000

    sorting out lists

--- src/components/PlaceDetails.js
+++ src/components/PlaceDetails.js
@@ -2,7 +2,17 @@ import React from 'react'
 
 const PlaceDetails = ({ place }) => {
   return (
-    <h1>{place.name}</h1>
+    <div className="place-card">
+
+      <div className="">
+        <h2>{place.name}</h2>
+        <img style={{height: 250}} src={place.photo? place.photo.images.large.url : 'https://images.unsplash.com/photo-1414235077428-338989a2e8c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cmVzdGF1cmFudHxlbnwwfHwwfHw%3D&w=1000&q=80'} alt="" />
+      </div>
+      <div className="place-card-footer">#
+          <div className="subtitle-1">{place.price_level}</div>
+          <div className="place-ranking">Ranking</div>
+      </div>
+    </div>
   )
 }
 
